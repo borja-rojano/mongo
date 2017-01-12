526b8dd78f83d35cac2053138548f5750e726476
borja-rojano <borja@tapewrite.com>
2017-01-12 11:14:26 +0100

    First Commit

---
+++ app.js
@@ -0,0 +1,45 @@
+"use strict";
+
+var mongo = require('mongodb').MongoClient,
+    assert =require('assert'),
+    express = require('express'),
+    engines= require('consolidate'),
+    app =express();
+
+app.engine('html', engines.nunjucks); //We register the nunjucks template engine to be associated with html extensions.
+app.set('view engine', 'html');       //Here we set the views engine of express to the 'html' engine we defined above.
+app.set('views', __dirname+'/views'); //Where our templates are located.
+
+
+
+
+mongo.connect('mongodb://localhost:27017/video', function (err, db) {
+    assert.equal(null, err);
+
+    console.log('Connected to DB');
+
+    app.get('/', function (req, res) {
+        res.render('index', {'name':'you'})
+    });
+
+    app.get('/movies', function (req, res) {
+        db.collection('movies').find({}).toArray(function (err, docs) {
+            console.log(docs);
+            res.render('movies', {'movies': docs})
+        });
+    });
+
+    app.use(function (req,res) {
+        res.send('There is nothing here');
+    });
+
+    var server = app.listen(3000, function () {
+        console.log("Server listening on port 3000\n");
+    });
+    
+});
+
+
+
+
+
